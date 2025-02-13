aac1ea83de8d4d34223832d6cf567a1cd6a0414a
FahmiYoshikage <fahmiilham029@gmail.com>
2025-02-13 16:35:44 +0700

    penyesuaian

--- server.js
+++ server.js
@@ -160,6 +160,6 @@ server.use((req, res, next) => {
 server.use(router);
 
 const port = 5000;
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`JSON Server is running on port ${port}`);
 });
